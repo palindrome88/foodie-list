15d013f2d6c1132e7abdd8adc675adf0f5c4483f
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-02-28 19:47:59 -0600

    finished up to 4

--- js/ToDOM.js
+++ js/ToDOM.js
@@ -5,9 +5,16 @@ let fetchesFood = require("./food-promises");
 let loadCards = document.getElementById("list-of-restaurants");
 let loadCardsCitiesSet = document.getElementById("list-of-cities");
 let testMe = document.getElementById("testme");
+let rests = document.getElementById("found-rests");
+// City Containers
+let cityContainer1 = [{}];
+let cityContainer2 =[{}];
 
+// Restaurant Containers
 let container = [{}];
-let container2 = [{}];
+let insideContainer =[{}];
+
+
 // Define what to do with the restaurants on the return of the promise.
 function getRestaurants(){
     let count = 0;
@@ -19,16 +26,14 @@ function getRestaurants(){
          
             container = resolve;
             let keys = Object.keys(container);
-
-            // container[i][0].id; <-- expose ID for item 0 "{id: 1, restaurant: "Schaefer, DuBuque and Satterfield", city_id: 4, date_visited: "6/25/2017", my_rating: 4.52}"
-            let insideContainer;
+    
             for (let i in container){
 
-                                //  container[i] <===== Whole Array
                 insideContainer = container[i];
                 count++;
                 for (let cnt in insideContainer){
                     //-----------------------------------------
+                    console.log(insideContainer[cnt]);
                     sortable.push([insideContainer[cnt], insideContainer[cnt].my_rating]);
                     sortable.sort(function (a, b){
                         return a[1] - b[1];
@@ -40,13 +45,10 @@ function getRestaurants(){
                 Id: ${sortable[i][0].id}</br>
                 Restarant: ${sortable[i][0].restaurant} </br>
                 Rating: ${sortable[i][0].my_rating} </br> 
-                
                 </br>
-
                 </div>`;
             }
     },
-
         (reject)=>{
             console.log("Something's wrong with getRestaurants()");
         }
@@ -57,30 +59,23 @@ function getCities(){
 
     fetchesFood.fetchCities().then(
 
-
         (resolve)=>{
-            container2 = resolve;
-            for (var t in container2){
-
-                let container3 = container2[t];
-                for(var u in container3){
-                    console.log(container3[u].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u].city}" id="select-${u}">
-                    ${container3[u].city}
+            cityContainer1 = resolve;
+            for (var t in cityContainer1){
+                cityContainer2 = cityContainer1[t];
+                for(var u in cityContainer2){
+                    console.log(cityContainer2[u].id);
+                    loadCardsCitiesSet.innerHTML += `<option value="${cityContainer2[u].city}" id="select-${u}">
+                    ${cityContainer2[u].city}
                     </option>`;
                 }
-
                 document.querySelector('select[name="list"]').onchange=changeEventHandler;
-         
         }
     },
         (reject)=>{
-
             console.log("ERROR, for fetchCities() in getCities() scope.");
         }
-        
     );
-
 }
 
 
@@ -90,10 +85,7 @@ function changeEventHandler(event) {
     if(!event.target.value) {console.log('Please Select One');}
     else  {
         let str = `${event.target.value}`;
-        console.log("It is working though:" + event.target.value);
-        console.log(str);
         testMe.innerHTML += str + "</br>";
-
         selectCities(str);
  }
 }
@@ -101,158 +93,128 @@ function changeEventHandler(event) {
 function selectCities(city){
     switch (city){
         case "Itupeva":
-            console.log("Itupeva is selected.");
-            for (var t0 in container2){
-
-                let container3 = container2[t0];
-                for(var u0 in container3){
-                    if (container3[u0].id === 0){
-                    console.log(container3[u0].city);
-                    testMe.innerHTML += `<option value="${container3[u0].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u0].city}
-                    </option>`;
-                    }
+        testMe.innerHTML = " ";      
+        console.log("Itupeva is selected.");
+        for (var t0 in insideContainer){
+                if (insideContainer[t0].city_id === cityContainer2[0].id)
+                {
+                    console.log(`Found for ${city}.`);
+                    console.log(insideContainer[t0]);
+                    testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t0].restaurant}, Last Date of Visit: ${insideContainer[t0].date_visited} </br>`;
                 }
-            }  
+            }
             break;
         case "Changsheng":
+        testMe.innerHTML = " ";
             console.log("Changsheng is selected.");
-            for (var t1 in container2){
-                let container3 = container2[t1];
-                for(var u1 in container3){
-                    if (container3[u1].id === 1){
-                    testMe.innerHTML += `<option value="${container3[u1].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u1].city}
-                    </option>`;
-                    }
+            for (var t1 in insideContainer){
+                if (insideContainer[t1].city_id === cityContainer2[1].id)
+                {
+                    console.log(`Found for ${city}.`);
+                    console.log(insideContainer[t1]);
+                    testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t1].restaurant}, Last Date of Visit: ${insideContainer[t1].date_visited}</br>`;
                 }
-            } 
+            }
             break;
         case "Mingelchaur":
-            for (var t3 in container2){
-
-                let container3 = container2[t3];
-                for(var u3 in container3){
-                    if (container3[u3].id === 3){
-                    console.log(container3[u3].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u3].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u3].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t2 in insideContainer){
+            if (insideContainer[t2].city_id === cityContainer2[2].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t2]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t2].restaurant}, Last Date of Visit: ${insideContainer[t2].date_visited}</br>`;
             }
+        }
             break;
         case "Bigoudine":
-            for (var t4 in container2){
-
-                let container3 = container2[t4];
-                for(var u4 in container3){
-                    if (container3[u4].id === 4){
-                    console.log(container3[u4].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u4].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u4].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t3 in insideContainer){
+            if (insideContainer[t3].city_id === cityContainer2[3].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t3]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t3].restaurant}, Last Date of Visit: ${insideContainer[t3].date_visited}</br>`;
             }
+        }
             break;
         case "La Soledad":
-            for (var t5 in container2){
-
-                let container3 = container2[t5];
-                for(var u5 in container3){
-                    if (container3[u5].id === 5){
-                    console.log(container3[u5].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u5].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u5].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t4 in insideContainer){
+            if (insideContainer[t4].city_id === cityContainer2[4].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t4]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t4].restaurant}, Last Date of Visit: ${insideContainer[t4].date_visited}</br>`;
             }
+        }
             break;
         case "Shengshan":
-            for (var t6 in container2){
-
-                let container3 = container2[t6];
-                for(var u6 in container3){
-                    if (container3[u6].id === 6){
-                    console.log(container3[u6].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u6].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u6].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t5 in insideContainer){
+            if (insideContainer[t5].city_id === cityContainer2[5].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t5]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t5].restaurant}, Last Date of Visit: ${insideContainer[t5].date_visited}</br>`;
             }
+        }
             break;
         case "Nashville":
-        for (var t7 in container2){
-
-            let container3 = container2[t7];
-            for(var u7 in container3){
-                if (container3[u7].id === 7){
-                console.log(container3[u7].city);
-                loadCardsCitiesSet.innerHTML += `<option value="${container3[u7].city}" onselect="printToDOM.getCities(city)">
-                ${container3[u7].city}
-                </option>`;
-                }
+        testMe.innerHTML = " ";
+        for (var t6 in insideContainer){
+            if (insideContainer[t6].city_id === cityContainer2[6].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t6]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t6].restaurant}, Last Date of Visit: ${insideContainer[t6].date_visited} </br>
+                HOMETOWN!!</br>`;
             }
         }
             break;
         case "Rabat":
-        for (var t8 in container2){
-
-            let container3 = container2[t8];
-            for(var u8 in container3){
-                if (container3[u8].id === 8){
-                console.log(container3[u8].city);
-                loadCardsCitiesSet.innerHTML += `<option value="${container3[u8].city}" onselect="printToDOM.getCities(city)">
-                ${container3[u8].city}
-                </option>`;
-                }
+        testMe.innerHTML = " ";  
+        for (var t7 in insideContainer){
+            if (insideContainer[t7].city_id === cityContainer2[7].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t7]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t7].restaurant}, Last Date of Visit: ${insideContainer[t7].date_visited}</br>`;
             }
         }
             break;
         case "Hamilton":
-            for (var t9 in container2){
-
-                let container3 = container2[t9];
-                for(var u9 in container3){
-                    if (container3[u9].id === 9){
-                    console.log(container3[u9].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u9].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u9].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t8 in insideContainer){
+            if (insideContainer[t8].city_id === cityContainer2[8].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t8]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t8].restaurant}, Last Date of Visit: ${insideContainer[t8].date_visited}</br>`;
             }
+        }
             break;
         case "El Llano":
-            for (var t10 in container2){
-
-                let container3 = container2[t10];
-                for(var u10 in container3){
-                    if (container3[u10].id === 10){
-                    console.log(container3[u10].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u10].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u10].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t9 in insideContainer){    
+            if (insideContainer[t9].city_id === cityContainer2[9].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t9]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t9].restaurant}, Last Date of Visit: ${insideContainer[t9].date_visited}</br>`;
             }
+        }
             break;  
         case "Longwu":
-            for (var t11 in container2){
-
-                let container3 = container2[t11];
-                for(var u11 in container3){
-                    if (container3[u11].id === 11){
-                    console.log(container3[u11].city);
-                    loadCardsCitiesSet.innerHTML += `<option value="${container3[u11].city}" onselect="printToDOM.getCities(city)">
-                    ${container3[u11].city}
-                    </option>`;
-                    }
-                }
+        testMe.innerHTML = " ";
+        for (var t10 in insideContainer){
+            if (insideContainer[t10].city_id === cityContainer2[10].id)
+            {
+                console.log(`Found for ${city}.`);
+                console.log(insideContainer[t10]);
+                testMe.innerHTML += `City: ${city}, Restaurant: ${insideContainer[t10].restaurant}, Last Date of Visit: ${insideContainer[t10].date_visited}</br>`;
             }
+        }
             break;
         default:
             break;
